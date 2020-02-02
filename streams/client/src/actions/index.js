32205544b8b3acdc26b30d649e77d5e80f6804f8
andrayantelo <andrayantelo@gmail.com>
2020-02-01 21:16:27 -0800

    Working on StreamDelete component. 	copied:     streams/client/src/components/streams/StreamCreate.js -> streams/client/src/components/streams/StreamForm.js

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN,
     SIGN_OUT,
@@ -22,9 +23,11 @@ export const signOut = () => {
     }
 };
 
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
     dispatch( {type: CREATE_STREAM, payload: response.data });
+    history.push('/');
 }
 
 export const fetchStreams = () => async dispatch => {
@@ -33,7 +36,7 @@ export const fetchStreams = () => async dispatch => {
 }
 
 export const fetchStream = (id) => async dispatch => {
-    const response = await streams.get(`/stream/${id}`);
+    const response = await streams.get(`/streams/${id}`);
     dispatch( {type: FETCH_STREAM, payload: response.data });
 }
 
