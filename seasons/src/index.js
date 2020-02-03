37f928b625acda5d91b91d85d8cf785b94095560
andrayantelo <andrayantelo@gmail.com>
2020-02-02 21:16:48 -0800

    Learned about hooks and rewrote an old app with functional components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,40 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay.js';
 import Spinner from './Spinner.js';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-    state = {lat: null, errorMessage: ""}
-
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({lat: position.coords.latitude}),
-            (error) => this.setState({errorMessage: error.message})
-          )
+const App = () => {
+    const [lat, errorMessage ] = useLocation();
+    let content;
+    if (errorMessage) {
+        content = <div>Error {errorMessage}</div>
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />;
+    } else {
+        content = <Spinner message="please accept location request"/>
     }
 
-    componentDidUpdate() {
-        console.log("My component was just updated.")
-    }
-
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-        else if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat}/>
-        }
-        else {
-            return <Spinner message="Please accept location request"/>
-        }
-    }
-    
-    render() {
-       return (
-           <div>
-               {this.renderContent()}
-            </div>)
-    }
-}
+    return (
+        <div className="border red">{content}</div>
+    )
+};
 
 ReactDOM.render(
     <App />,
